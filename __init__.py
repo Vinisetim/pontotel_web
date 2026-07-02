4bb7f8bb0586f359f30577493c2cfa229061e090
vinicius.gomes <viniciusgueimis@gmail.com>
2026-07-02 14:19:24 -0300

    testes selenium

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
-import selenium
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = selenium.webdriver.Chrome()
+driver = webdriver.Chrome()
+
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
 title = driver.title
@@ -9,12 +10,14 @@ title = driver.title
 driver.implicitly_wait(0.5)
 
 text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_buton = driver.find_element(by=By.CLASS_NAME, value="button")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
 text_box.send_keys("Selenium")
-submit_buton.click()
+submit_button.click()
 
-message = driver.find_element(by=By.CLASS_NAME, value="message")
+message = driver.find_element(by=By.ID, value="message")
 text = message.text
 
 driver.quit()
+
+print(text)
